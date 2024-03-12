7353b988964d09a797944ca992784d5025dfc036
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-12 14:46:44 +0900

    [문제 2] array<>

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,57 @@
 #include <iostream>
+#include <random>
+#include <print>
+#include <algorithm>
+#include <array>
 #include "save.h"
 
-
 /**
-* [문제] main을 변경하지 않고 의도대로 실행되도록 change()을 선언하고 정의하라.
+* [문제 2] int 100개를 저장할 공간을 만들어라.
+* [0, 99'999] 범위의 값을 int 100개를 할당하라.
+* TODO: 화면에 값을 출력한다.
+* 
 */
 
-class Dog {
-private:
-	int value;
+// 용량이 크기 때문에 STACK 말고 DATA에 하나만 만들어서 돌려 사용한다.
+std::default_random_engine dre;
+std::uniform_int_distribution uid{ 0, 99'999 };
 
-public:
-	Dog(int _value)
-		: value(_value)
-	{}
-	
-	 friend std::ostream& operator<<(std::ostream& out, const Dog& dog) {
-		out << dog.value;
-		return out;
-	}
-};
-
-
-// 템플릿 함수 선언
-template <class T>
-void change(T&, T&);
-
-// 함수의 이름은 주소
-// 함수의 시작 주소가 CODE영역에 있다.
 int main()
 {
-	// 지역 변수는 STACK에 생성된다.
-	Dog a{ 1 }, b{ 2 };
-	// 컴파일러가 함수를 찾는 순서
-	// 1. change(Dog, Dog)
-	// 2. change(Dog&, Dog&)
-	// * 이때, 1과 2중 하나만 사용해야 한다. (모호함)
-	// 3. 템플릿 코드 확장 
-	//
+	std::array<int, 100> a; // 앞으로 T[N]은 사용하지 않는다. -> array<T, N> 사용
 
-	// CODE에 있는 change 함수로 가기 전 STACK에 진행상황 저장
-	change(a, b);
+	sizeof a;
 
-	std::cout << a << " " << b << std::endl;
-	save("src\\main.cpp");
-}
+	for (auto& num : a) {
+		num = uid(dre);
+	}
 
-// 템플릿 함수 정의
-// but 일반적으로 템플릿 함수의 선언과 정의는 한번에 작성한다.
-// Generic code
-template <class T>
-void change(T& a, T& b)
-{
-	std::swap(a, b);
+	for (const auto num : a) {
+		std::print("{:8}", num);
+		// std::print(std::cout, "{:8}", a[i]);
+		// 내가 cout 말고 다른 ostream을 사용하고 싶으면 cout 자리에 명시한다.
+	}
+	std::cout << std::endl;
+
+	// [문제 2-1] a의 값중 가장 큰 값을 찾아 출력하시오.
+
+	// stl 사용 전의 코드.
+	auto max = a[0];
+	// auto max = std::numeric_limits<int>::min();
+	for (const auto num : a) {
+		if (num > max) { max = num; }
+	}
+	std::println("Max Value: {}", max);
+	
+	// stl 사용 이후 코드.
+	std::println("Max Value: {}", *std::max_element(a.begin(), a.end()));
+	// 이때 end는 마지막 원소의 다음이다.
+
+	// stl을 사용하는 이유
+	// 1. stl 코드는 최적의 성능을 가지도록 되어 있다.
+	// 2. 손으로 짠 코드에서 발생할 수 있는 오류를 해결
+	// 3. 코드 작성 속도 향상
+
+
+	// save("src\\main.cpp");
 }
