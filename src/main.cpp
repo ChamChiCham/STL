00cba91bffa7df311b02ccd25e6e2b0506f92a89
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-19 15:10:12 +0900

    [문제 6-2] count_if로 숫자 세기

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 #include "save.h"
 
 /**
-* [문제 6] "개들"에는  class Dog객체 100개 기록되어 있다.
+* [문제 6-2] "개들"에는  class Dog객체 100개 기록되어 있다.
 * 파일은 binary모드이고 모든 객체를 한번에 write 함수로 기록하였다.
-* 파일을 읽어 가장 num값이 큰 Dog 정보를 화면에 출력하라.
+* 파일을 읽어 가장 num값이 10'000미만인 Dog 객체는 몇 개인지 화면에 출력하라.
 * class Dog의 멤버는 다음과 같다.
+* 
+* 다음 시간-:: 실행파일의 메모리 구조 - STACK, CODE, DATA, Free Store(HEAP)
 */
 
 class Dog {
@@ -40,26 +42,24 @@ int main()
 	// 100번째 Dog, c: i num: -2228
 
 	std::array<Dog, 100> dogs;
-	in.read((char*)&dogs, sizeof(Dog) * dogs.size());
+	in.read((char*)dogs.data(), sizeof(Dog) * dogs.size());
 
-	// 아래와 같이 코딩하는 것은 잘못된 코드이다.
-	/*Dog maxDog;
-	int maxValue{ std::numeric_limits<int>::min() };
+	// 이런 식으로 작성하면 빵점이래
+	/*int cnt{};
 	for (const auto& dog : dogs) {
-		if (maxValue < dog.getNum()) {
-			maxDog = dog;
-			maxValue = dog.getNum();
-		}
+		if (dog.getNum() < 10'000) { cnt++; }
 	}*/
 
-	
-	// 누가 더 큰지 판단하는 함수를 만들어야 한다. => 람다 함수
-	auto pos = std::max_element(dogs.begin(), dogs.end(), [](const Dog& lhs, const Dog& rhs) -> bool {
-		return lhs.getNum() < rhs.getNum();
-		// 이딴식으로 쓰지 말래
-		}) /* ->show() */ ;
+	auto cnt{ std::count_if(dogs.begin(), dogs.end(), [](const Dog& dog) {
+		if (10'000 > dog.getNum()) {
+			return true;
+		}
+		return false;
+		}) };
+	std::cout << cnt << std::endl;
 
-	pos->show();
+
+
 	// save("src\\main.cpp");
 }
 
