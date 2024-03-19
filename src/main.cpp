f48d6c87f066486c39c9c68e531ad47745ca93d1
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-19 14:46:59 +0900

    [문제 6] 가장 큰 값 출력

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <fstream>
 #include <iostream>
 #include <array>
+#include <algorithm>
 #include "save.h"
 
 /**
-* [문제 6 변형] "개들"에는 몇개인지 모르는 class Dog객체가 기록되어 있다.
+* [문제 6] "개들"에는  class Dog객체 100개 기록되어 있다.
 * 파일은 binary모드이고 모든 객체를 한번에 write 함수로 기록하였다.
 * 파일을 읽어 가장 num값이 큰 Dog 정보를 화면에 출력하라.
 * class Dog의 멤버는 다음과 같다.
@@ -12,10 +13,13 @@
 
 class Dog {
 private:
-	char c;
-	int num;
+	char c{};
+	int num{};
 
 public:
+
+	int getNum() const { return num; }
+
 	void show() const {
 		std::cout << "Dog: C:" << c << " NUM:" << num << std::endl;
 	}
@@ -35,18 +39,27 @@ int main()
 	
 	// 100번째 Dog, c: i num: -2228
 
+	std::array<Dog, 100> dogs;
+	in.read((char*)&dogs, sizeof(Dog) * dogs.size());
 
-	// in 파일의 크기를 filesystem을 이용하여 얻는다.
-	// 파일 사이즈/sizeof(Dog) => 100개
-	// 개수를 모를 떈 다음과 같은 방법으로 코딩 new Dog[num];
+	// 아래와 같이 코딩하는 것은 잘못된 코드이다.
+	/*Dog maxDog;
+	int maxValue{ std::numeric_limits<int>::min() };
+	for (const auto& dog : dogs) {
+		if (maxValue < dog.getNum()) {
+			maxDog = dog;
+			maxValue = dog.getNum();
+		}
+	}*/
 
-	Dog dog;
-	int count{};
-	while (in >> dog) { count++; }
+	
+	// 누가 더 큰지 판단하는 함수를 만들어야 한다. => 람다 함수
+	auto pos = std::max_element(dogs.begin(), dogs.end(), [](const Dog& lhs, const Dog& rhs) -> bool {
+		return lhs.getNum() < rhs.getNum();
+		// 이딴식으로 쓰지 말래
+		}) /* ->show() */ ;
 
-	std::cout << "읽은 Dog 개수" << count << std::endl;
-	dog.show();
-
-	save("src\\main.cpp");
+	pos->show();
+	// save("src\\main.cpp");
 }
 
