d42a921d7595824ae34864d1c2c8162f5eae8bb3
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-21 15:13:14 +0900

    DATA 영역의 한계

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,29 @@
 using std::cout;
 using std::endl;
 
+// default 생성자를 이용해 초기화 된다.
+// int g_a[100];
 
-int num;
+// 초기화하지 않은 전역 data (bss)
+// DATA 1MB -> int{} 호출 -> 실행될 때 default 초기화
+char g_a[1'000'000];
+// 초기화한 전역 data
+// [0] -> {1}, 나머지 DATA -> int{} 호출 ->
+// 메모리의 모양 이미지를 만들어서 실행파일에 그대로 저장한다.
+char g_a0[1'000'000]{ 1 };
+
+
 
 int main()
 {
-	
-	int num;
-	int* p = new int;
-	
-	cout << "     STACK " << std::addressof(num) << endl;
-	cout << endl;
+	// 메모리에 할당만 함. 초기화 X
+	// int a[100];
 
-	cout << "Free Store " << std::addressof(*p) << endl;
-	cout << endl;
+	// Stack overflow
+	// int a[350'000];
+	cout << g_a0[999999] << endl;
+	cout << g_a0[0] << endl;
 
-	cout << "      CODE " << std::addressof(main) << endl;
-	cout << endl;
-
-	cout << "      DATA " << std::addressof(::num) << endl;
-	cout << endl;
-
-
-	delete p;
-	save("src\\main.cpp");
+	// save("src\\main.cpp");
 }
 
