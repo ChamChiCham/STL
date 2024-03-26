d5d7630a901cb3fbc6c756c6744b682b17c51de8
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-26 14:44:45 +0900

    [문제 7] Free Store to T*(raw*)

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
-#include <array>
-#include <string>
+#include <numeric>
+#include <thread>
 #include "save.h"
 
 /**
-* 실행파일의 메모리 구조 - STACK, CODE, DATA, Free Store(HEAP)
+* [문제 7] int num 값을 입력받으시오.
+* free store에서 int를 num개 할당받아라
+* int값을 1부터 시작하는 정수로 채워라.
+* int값의 합계를 출력하라
+* 이 과정을 영원히 반복하라.
+* 해결하는 코드를 답지에 적어라.
+* 
+* free store - RAII
+* 
+* c++언어에서 사용하지 않도록 권고
+* 1. char*		-> std::string
+* 2. T[N]		-> std::array<T,N>
+* 3. T*(raw*)	-> unique_ptr, shared_ptr(스마트 포인터)
 */
-
-using std::cout;
-using std::endl;
-
-// default 생성자를 이용해 초기화 된다.
-// int g_a[100];
-
-// 초기화하지 않은 전역 data (bss)
-// DATA 1MB -> int{} 호출 -> 실행될 때 default 초기화
-char g_a[1'000'000];
-// 초기화한 전역 data
-// [0] -> {1}, 나머지 DATA -> int{} 호출 ->
-// 메모리의 모양 이미지를 만들어서 실행파일에 그대로 저장한다.
-char g_a0[1'000'000]{ 1 };
-
-
 
 int main()
 {
-	// 메모리에 할당만 함. 초기화 X
-	// int a[100];
+	
+	while (true) {
+		long long num;
+		std::cin >> num;
+		// 컴파일 타임에는 알 수 없는 값.
+		// 속도가 걸림
+		int* data{ nullptr };
+		data = new int[num]{};
+		std::iota(data, data + num, 1);
+		long long sum{ std::accumulate(data, data + num, 0LL) };
+		std::cout << sum << std::endl;
+		delete[] data;
 
-	// Stack overflow
-	// int a[350'000];
-	cout << g_a0[999999] << endl;
-	cout << g_a0[0] << endl;
+	}
 
-	// save("src\\main.cpp");
+
+
+	//save("src\main.cpp");
 }
 
