7e78cfbd52fa2f892b3ff3dadcbea5521fdd2f1a
ChamChiCham@C106 <asd@asd>
2024-04-16 14:11:49 +0900

    벡터가 이사갈 경우의 변화

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,26 @@
 /**
 * STL container - Containers are objects that store other objects
 * 
-* 이동할 때 예외가 발생하지 않음을 보장 - noexcept
+* push_back(T) - amortizer O(1)
 */
 
 extern bool check;
 
 int main()
 {
-	// push_back의 이동, 복사 순서 확인하기
-
-	check = true;
 	
-	// 소멸자가 오름차순으로 호출됨.
-	std::vector<String> v;
-	v.reserve(5);
+	std::vector<int> v;
+	std::cout << "개수 - " << v.size() << std::endl;
+	std::cout << "주소 - " << v.data() << std::endl;
+	std::cout << "최대 - " << v.capacity() << std::endl;
 
-	v.emplace_back("벡터입니다.");
-	v.emplace_back("벡터는 dynamic Array입니다.");
-	v.emplace_back("벡터는 dynamic Array입니다.");
-	v.emplace_back("벡터는 dynamic Array입니다.");
-	v.emplace_back("벡터는 dynamic Array입니다.");
+	v.reserve(123);
+	std::cout << "개수 - " << v.size() << std::endl;
+	std::cout << "주소 - " << v.data() << std::endl;
+	std::cout << "최대 - " << v.capacity() << std::endl;
 
-	// 소멸자가 내림차순으로 호출됨.
-	std::array<String, 5> ar;
 
-	std::cout << "main end" << std::endl;
+	
+
+
 }	
