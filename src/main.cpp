b1519c2acc6d8175dbab0f18173481fd18b02448
ChamChiCham@C106 <asd@asd>
2024-04-16 15:01:58 +0900

    [문제] 홀수 제거

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+#include <print>
+#include <numeric>
 #include "save.h"
 #include "string.h"
 
@@ -15,30 +17,22 @@ extern bool check;
 
 int main()
 {
-	// uniform initialization 
-	std::vector<int> v{ 1, 2, 3, 4, 5, 3, 3, 3};
+	std::vector<int> v(100);
 
-	// initialize_list
-	for (int num : {1, 2, 3, 4, 5}) {
-		std::cout << num << std::endl;
-	}
-
-	// [문제] v에서 3을 제거하라.
-
-	// 벽이 쳐져있어서 반복자로만 대화할수 있다.
-	// 한칸 떙긴 iterator을 리턴해서
-	// 한칸 떙겨야 한다는 정보를 준다.
-	// 실제 지우는건 벡터가 지워야 한다.
-	// erase-remove idiom이라고 부름
+	std::iota(v.begin(), v.end(), 1);
 	
-	// v.erase(std::remove(v.begin(), v.end(), 3), v.end());
-
-	// c++20
-	std::erase(v, 3);
-
-	for (const auto& num : v) {
-		std::cout << num << std::endl;
+	for (int num : v) {
+		std::print("{:8}", num);
 	}
 	
-	
+	std::println("");
+
+	// [문제] v에서 홀수를 제거하라.
+	std::erase_if(v, [](int x) {
+		return x & 1;
+		});
+
+	for (int num : v) {
+		std::print("{:8}", num);
+	}
 }	
